1c499d2db66477e9ce15f9922791b00699f681a9
MOHABEN48 <medoubenouda@gmail.com>
2025-02-25 11:00:08 +0000

    la premier modificastion 2

---
+++ includes/config.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost"; // Serveur MySQL (XAMPP)
+$user = "root";      // Par défaut, XAMPP utilise "root"
+$password = "";      // Par défaut, pas de mot de passe
+$dbname = "gestion"; // Nom de ta base de données
+
+// Connexion à la base de données
+$conn = new mysqli($host, $user, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Échec de la connexion : " . $conn->connect_error);
+}
+
+// Définir l'encodage des caractères en UTF-8
+$conn->set_charset("utf8");
+?>
